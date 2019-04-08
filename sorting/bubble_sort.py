df6d463b61eabafdfe69d75c94bb4e858a588e98
Ethan-zhengyw <zhengyw55@gmail.com>
2019-04-08 14:26:51 +0800

    Update bubble_sort.py
    Should break loop if no swap action taken.

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,5 +1,11 @@
 def bubble_sort(array):
     for i in range(len(array) - 1):
+        swapped = False
+
         for j in range(i + 1, len(array)):
             if array[i] > array[j]:
                 array[i], array[j] = array[j], array[i]
+                swapped = True
+
+        if not swapped:
+            break
